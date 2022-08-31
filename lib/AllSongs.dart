45c78b3db8c9b847d44b57a18a0e7f21eded395c
cdmi123 <languagepdf@gmail.com>
2022-08-31 09:28:39 +0530

    Initial commit

---
+++ lib/AllSongs.dart
@@ -0,0 +1,31 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:demo_musicplayer/Model.dart';
+import 'package:flutter/material.dart';
+
+class AllSongs extends StatefulWidget {
+  const AllSongs({Key? key}) : super(key: key);
+
+  @override
+  State<AllSongs> createState() => _AllSongsState();
+}
+
+class _AllSongsState extends State<AllSongs> {
+
+  AudioPlayer player = AudioPlayer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("All Songs"),),
+      body: ListView.builder(itemBuilder: (context, index) {
+        return ListTile(
+          onTap: () async {
+            await player.play(DeviceFileSource(Model.songlist[index].data));
+          },
+          title: Text("${Model.songlist[index].displayNameWOExt}"),
+          subtitle: Text("${Model.songlist[index].duration}"),
+        );
+      },),
+    );
+  }
+}
