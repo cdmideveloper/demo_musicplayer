62617ebe59656d1d26b1bebcb8100ccce513a510
cdmi123 <languagepdf@gmail.com>
2022-09-01 09:22:56 +0530

    Initial commit

--- lib/AllSongs.dart
+++ lib/AllSongs.dart
@@ -10,22 +10,56 @@ class AllSongs extends StatefulWidget {
 }
 
 class _AllSongsState extends State<AllSongs> {
-
   AudioPlayer player = AudioPlayer();
+
+  List<bool> statuslist = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    statuslist = List.filled(Model.songlist.length, false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("All Songlist"),),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return ListTile(
-          onTap: () async {
-            await player.play(DeviceFileSource(Model.songlist[index].data));
-          },
-          title: Text("${Model.songlist[index].displayNameWOExt}"),
-          subtitle: Text("${Model.songlist[index].duration}"),
-        );
-      },),
+      appBar: AppBar(
+        title: Text("All Songlist"),
+      ),
+      body: ListView.builder(
+        itemCount: Model.songlist.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            trailing: statuslist[index]
+                ? IconButton(
+                    onPressed: () async {
+                      setState(() {
+                        statuslist = List.filled(Model.songlist.length, false);
+                      });
+                      print(statuslist);
+                      await player.stop();
+                      },
+                    icon: Icon(Icons.pause))
+                : IconButton(
+                    onPressed: () async {
+                      await player.stop();
+                      setState(() {
+                        statuslist = List.filled(Model.songlist.length, false);
+                        statuslist[index] = true;
+                      });
+                      await player
+                          .play(DeviceFileSource(Model.songlist[index].data));
+
+                      print(statuslist);
+                    },
+                    icon: Icon(Icons.play_arrow)),
+            title: Text("${Model.songlist[index].displayNameWOExt}"),
+            subtitle: Text("${Model.songlist[index].duration}"),
+          );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,90 @@
 import 'package:demo_musicplayer/AllSongs.dart';
 import 'package:demo_musicplayer/Model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
+//131294
 void main() {
   runApp(MaterialApp(
-    home: splash(),
+    home: demo(),
   ));
+}
+
+class demo extends StatefulWidget {
+  const demo({Key? key}) : super(key: key);
+
+  @override
+  State<demo> createState() => _demoState();
+}
+
+class _demoState extends State<demo> {
+  String curdate = "";
+  String curTime = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    curdate = DateTime.now().toString();
+    curTime = TimeOfDay.now().toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DatePicker and TimePicker"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2025))
+                    .then((value) {
+                  if (value != null) {
+                    setState(() {
+                      curdate = value.toString();
+                    });
+                  }
+                });
+              },
+              child: Text("${curdate}")),
+          ElevatedButton(
+              onPressed: () {
+                showTimePicker(
+                        initialEntryMode: TimePickerEntryMode.input,
+                        context: context,
+                        initialTime: TimeOfDay.now())
+                    .then((value) {
+                  if (value != null) {
+                    setState(() {
+                      curTime = value.toString();
+                    });
+                  }
+                });
+              },
+              child: Text("${curTime}")),
+         ElevatedButton(onPressed: () {
+           showModalBottomSheet(builder: (context) {
+             return  Container(
+               height: 150,
+               child: CupertinoDatePicker(mode: CupertinoDatePickerMode.time,onDateTimeChanged: (value) {
+                 print(value);
+               },),
+             );
+           },context: context);
+         }, child: Text("Time"))
+        ],
+      ),
+    );
+  }
 }
 
 class splash extends StatefulWidget {
